c25a8defcaa45abe7488f68a5edd931f257f117f
xudelong@100tal.com <xudelong@100tal.com>
2019-08-21 15:36:10 +0800

    edit

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ const app = require('express')();
 app.get('/', (req, res) => {
     res.send('Welcome');
 });
+app.get('/asd', (req, res) => {
+    res.send('asd');
+});
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
